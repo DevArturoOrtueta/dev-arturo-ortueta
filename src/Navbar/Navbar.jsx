2a276e9a99a32f933b32f34ce34e0c902a58a133
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2024-04-04 16:16:57 -0500

    Update

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import './Navbar.css'
+
 export const Navbar = () => {
   return (
     <div className='navbar'>
         <div className='navbar-logo'>
-
+            A
         </div>
         <div className='navbar-menu'>
             <ul>
                 <li>
                     <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/services'>Services</Link>
+                </li>
+                <li>
+                    <Link to='/services'>Services</Link>
+                </li>
+                <li>
+                    <Link to='/services'>Services</Link>
                 </li>
                 <li>
                     <Link to='/services'>Services</Link>
