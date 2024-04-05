ef7fbf80bf71f67788b3dd7ccedebc61748913d0
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2024-04-05 13:03:48 -0500

    Update

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -4,6 +4,31 @@ import './Home.css'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div  className='home-main'>
+      <div className='home-hero'>
+        <div className='home-hero-aboutme'>
+          <div className='aboutme-img'>
+
+          </div>
+          <div className='aboutme-body'>
+
+          </div>
+        </div>
+        <div className='home-hero-skills'>
+            <div className=''>
+
+            </div>
+        </div>
+      </div>
+      <div className='home-services'>
+
+      </div>
+      <div className='home-feedback'>
+
+      </div>
+      <div className='home-contact'>
+
+      </div>
+    </div>
   )
 }
