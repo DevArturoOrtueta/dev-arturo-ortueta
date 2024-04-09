c8ae92c585fa74c4155be1a79e7c098c0831085a
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2024-04-08 22:18:52 -0500

    Update general font-family, add some things in not page found

--- src/components/NotFound/NotPage.jsx
+++ src/components/NotFound/NotPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import './NotPage.css'
 import ImgVoid from '../../resources/img/ImgVoid.png'
+import { Link } from 'react-router-dom'
 
 export const NotPage = () => {
   return <div className='notfound'>
     <img className='notfound-img' src={ImgVoid} alt='PageNotFound'/>
+    <h1>Ups veo que estas perdido - <Link to='/'>Click aquí para ir al Home</Link></h1>
   </div>
 }
