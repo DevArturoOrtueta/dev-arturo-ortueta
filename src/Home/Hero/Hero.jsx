5d99e6a6561661f891486e28cafeb020648152e9
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2024-04-09 21:44:10 -0500

    Hero component added, also ScrollDon structure added

--- src/Home/Hero/Hero.jsx
+++ src/Home/Hero/Hero.jsx
@@ -4,6 +4,13 @@ import React from 'react'
 
 export const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='hero-main'>
+        <div className='hero-img'>
+
+        </div>
+        <div className='hero-body'>
+            
+        </div>
+    </div>
   )
 }
